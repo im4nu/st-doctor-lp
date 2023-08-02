e12955671327f904c68b77b15719939fbb82e817
im4nu <victorx811@gmail.com>
2023-08-02 11:12:46 -0300

    starttec link added on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -83,7 +83,7 @@ export default function Footer() {
         </div>
 
         <div className="flex flex-col items-start text-start gap-3 w-full lg:w-1/6">
-          <a href="#">
+          <a href="https://www.starttec.com.br/">
             <Image
               src={"/images/startTec-logo.png"}
               width={150}
